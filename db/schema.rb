fe5c30869de462390194a9d8ea910c7f083ab3f5
Michael Cittar <mcittar@Michaels-MacBook-Air.local>
2017-02-16 10:38:04 -0800

    Create album table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216183353) do
+ActiveRecord::Schema.define(version: 20170216183536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string  "title",   null: false
+    t.integer "band_id", null: false
+  end
+
+  add_index "albums", ["band_id"], name: "index_albums_on_band_id", using: :btree
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
