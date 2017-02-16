c3b10f6ec924bf340e647d8d8bb31be3486ed291
Michael Cittar <mcittar@Michaels-MacBook-Air.local>
2017-02-16 10:35:21 -0800

    Create bands table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216171912) do
+ActiveRecord::Schema.define(version: 20170216183353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bands", force: :cascade do |t|
+    t.string "name", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email",           null: false
